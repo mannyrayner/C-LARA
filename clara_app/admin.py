bddfd1f85dba908018874bbc85af96ba4238a736
Manny Rayner <mannyrayner@yahoo.com>
2023-07-30 20:18:25 +0930

    Temporarily remove custom User model

--- clara_app/admin.py
+++ clara_app/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 #from .models import User
 from django.contrib.auth.models import User
 
-admin.site.register(User)
+# Temporarily remove User
+#admin.site.register(User)
 
 
 
