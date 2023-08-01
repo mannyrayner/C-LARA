67106cb085d767cf89848478b7a927f6e77b82ce
Manny Rayner <mannyrayner@yahoo.com>
2023-08-01 11:48:50 +0930

    Try to correct issue with AWS_S3_ENDPOINT_URL

--- clara_project/settings.py
+++ clara_project/settings.py
@@ -179,7 +179,8 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
+STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
