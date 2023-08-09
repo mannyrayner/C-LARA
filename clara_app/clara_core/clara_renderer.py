08a29544b9b65c744d867a36fec72a5600e2b983
Manny Rayner <mannyrayner@yahoo.com>
2023-08-09 12:00:56 +0930

    Rationalise file copying in rendering operation

--- clara_app/clara_core/clara_renderer.py
+++ clara_app/clara_core/clara_renderer.py
@@ -112,8 +112,8 @@ class StaticHTMLRenderer:
                         post_task_update(callback, f'--- Copied {n_files_copied}/{n_files_to_copy} files')
                 except Exception as e:
                     post_task_update(callback, f'*** Warning: error when copying audio from {old_audio_file_path} to {new_audio_file_path}')
-                    error_message = f'"{str(e)}"\n{traceback.format_exc()}'
-                    post_task_update(callback, error_message)
+                    #error_message = f'"{str(e)}"\n{traceback.format_exc()}'
+                    #post_task_update(callback, error_message)
             post_task_update(callback, f"--- Done. {n_files_copied}/{n_files_to_copy} files successfully copied")
                         
         total_pages = len(text.pages)
