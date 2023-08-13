58ccdac74d82af3525a52bb99d0119cccc4045de
Manny Rayner <mannyrayner@yahoo.com>
2023-08-14 09:12:54 +0930

    Make TTS repo consistent

--- clara_app/clara_core/clara_tts_repository.py
+++ clara_app/clara_core/clara_tts_repository.py
@@ -17,8 +17,8 @@ The function clara_database_adapter.localise_sql_query converts this to sqlite3 
 from .clara_database_adapter import connect, localise_sql_query
 
 from .clara_tts_api import get_tts_engine_types
-from .clara_utils import _s3_storage, get_config, absolute_file_name, absolute_local_file_name, file_exists, copy_local_file
-from .clara_utils import make_directory, remove_directory, directory_exists, list_files_in_directory, post_task_update
+from .clara_utils import _s3_storage, get_config, absolute_file_name, absolute_local_file_name, file_exists, local_file_exists, copy_local_file
+from .clara_utils import make_directory, remove_directory, directory_exists, local_directory_exists, make_local_directory, list_files_in_directory, post_task_update
 
 from .clara_classes import InternalCLARAError
 
@@ -30,37 +30,55 @@ import traceback
 config = get_config()
 
 class TTSRepository:
-    def __init__(self):   
-        self.db_file = absolute_local_file_name(config.get('tts_repository', 'db_file_s3' if _s3_storage else 'db_file'))
+    def __init__(self, callback=None):   
+        self.db_file = absolute_local_file_name(config.get('tts_repository', 'db_file'))
         self.base_dir = absolute_file_name(config.get('tts_repository', 'base_dir'))
-        self._initialize_repository()
+        self._initialize_repository(callback=callback)
 
-    def _initialize_repository(self):
+    def _initialize_repository(self, callback=None):
         if not directory_exists(self.base_dir):
-            make_directory(self.base_dir)
+            make_directory(self.base_dir, parents=True, exist_ok=True)
 
-        connection = connect(self.db_file)
-        cursor = connection.cursor()
-        if os.getenv('DB_TYPE') == 'sqlite':
-            cursor.execute('''CREATE TABLE IF NOT EXISTS metadata
-                              (id INTEGER PRIMARY KEY,
-                               engine_id TEXT,
-                               language_id TEXT,
-                               voice_id TEXT,
-                               text TEXT,
-                               file_path TEXT)''')
-        # Assume Postgres, which does auto-incrementing differently
-        # We need a suitable definition for the primary key
-        else:
-            cursor.execute('''CREATE TABLE IF NOT EXISTS metadata
-                              (id SERIAL PRIMARY KEY,
-                               engine_id TEXT,
-                               language_id TEXT,
-                               voice_id TEXT,
-                               text TEXT,
-                               file_path TEXT)''')
-        connection.commit()
-        connection.close()
+        try:
+            if os.getenv('DB_TYPE') == 'sqlite':
+                # If we're using sqlite, check if db_file exists and if not create it
+                db_dir = config.get('tts_repository', 'db_dir')
+                post_task_update(callback, f'--- Creating empty DB file for TTS repository, {self.db_file}')
+                if not local_directory_exists(db_dir):
+                    make_local_directory(db_dir)
+                if not local_file_exists(self.db_file):
+                    open(self.db_file, 'a').close()
+                    
+            connection = connect(self.db_file)
+            cursor = connection.cursor()
+        
+            if os.getenv('DB_TYPE') == 'sqlite':
+                cursor.execute('''CREATE TABLE IF NOT EXISTS metadata
+                                  (id INTEGER PRIMARY KEY,
+                                   engine_id TEXT,
+                                   language_id TEXT,
+                                   voice_id TEXT,
+                                   text TEXT,
+                                   file_path TEXT)''')            
+            # Assume Postgres, which does auto-incrementing differently
+            # We need a suitable definition for the primary key
+            else:
+                cursor.execute('''CREATE TABLE IF NOT EXISTS metadata
+                                  (id SERIAL PRIMARY KEY,
+                                   engine_id TEXT,
+                                   language_id TEXT,
+                                   voice_id TEXT,
+                                   text TEXT,
+                                   file_path TEXT)''')
+                                   
+            connection.commit()
+            connection.close()
+            post_task_update(callback, f'--- Initialised TTS repository')
+                                   
+        except Exception as e:
+            error_message = f'*** Error when trying to initialise TTS database: "{str(e)}"\n{traceback.format_exc()}'
+            post_task_update(callback, error_message)
+            raise InternalCLARAError(message='TTS database inconsistency')   
 
     def delete_entries_for_language(self, language_id):
         connection = connect(self.db_file)
@@ -88,19 +106,22 @@ class TTSRepository:
         try:
             connection = connect(self.db_file)
             cursor = connection.cursor()
-            # cursor.execute(localise_sql_query("SELECT file_path FROM metadata WHERE engine_id = %s AND language_id = %s AND voice_id = %s AND text = %s"),
-                           # (engine_id, language_id, voice_id, text))
-            cursor.execute("""SELECT file_path FROM metadata 
-                              WHERE engine_id = %(engine_id)s 
-                              AND language_id = %(language_id)s 
-                              AND voice_id = %(voice_id)s 
-                              AND text = %(text)s""",
-                           {
-                              'engine_id': engine_id,
-                              'language_id': language_id,
-                              'voice_id': voice_id,
-                              'text': text
-                           })
+            if os.getenv('DB_TYPE') == 'sqlite':
+                cursor.execute("SELECT file_path FROM metadata WHERE engine_id = ? AND language_id = ? AND voice_id = ? AND text = ?",
+                               (engine_id, language_id, voice_id, text))
+            else:
+                # Assume postgres
+                cursor.execute("""SELECT file_path FROM metadata 
+                                  WHERE engine_id = %(engine_id)s 
+                                  AND language_id = %(language_id)s 
+                                  AND voice_id = %(voice_id)s 
+                                  AND text = %(text)s""",
+                               {
+                                  'engine_id': engine_id,
+                                  'language_id': language_id,
+                                  'voice_id': voice_id,
+                                  'text': text
+                               })
             result = cursor.fetchone()
             connection.close()
             if os.getenv('DB_TYPE') == 'sqlite':
