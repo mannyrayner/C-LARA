f75ad306bc4e59fd01434ddfadd764633ab1647f
Manny Rayner <mannyrayner@yahoo.com>
2023-09-06 17:15:12 +0930

    Make CEFR an independent function

--- clara_app/urls.py
+++ clara_app/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('project/<int:project_id>/clone_project/', views.clone_project, name='clone_project'),  
     path('project/<int:project_id>/create_plain_text/', views.create_plain_text, name='create_plain_text'),
     path('project/<int:project_id>/create_summary/', views.create_summary, name='create_summary'),
+    path('project/<int:project_id>/create_cefr_level/', views.create_cefr_level, name='create_cefr_level'),
     path('project/<int:project_id>/create_segmented_text/', views.create_segmented_text, name='create_segmented_text'),
     path('project/<int:project_id>/create_glossed_text/', views.create_glossed_text, name='create_glossed_text'),
     path('project/<int:project_id>/create_lemma_tagged_text/', views.create_lemma_tagged_text, name='create_lemma_tagged_text'),
