74e1bd8cd1587575888bb1651b1e8be27022fe65
Manny Rayner <mannyrayner@yahoo.com>
2023-10-07 13:15:39 +1030

    Images

--- clara_app/clara_core/clara_image_repository.py
+++ clara_app/clara_core/clara_image_repository.py
@@ -1,3 +1,36 @@
+"""
+clara_image_repository.py
+
+This module implements an image repository that stores image files and associated metadata. The code is based
+on the earlier class AudioRepository, defined in clara_audio_repository.py.
+
+Classes:
+- ImageRepository: Class for managing the audio repository.
+
+The ImageRepository class provides methods for adding entries, retrieving entries,
+getting the image directory, and storing image files.
+
+Use clara_database_adapter so that the code works for both sqlite3 and PostgreSQL databases.
+
+SQL templates must be written in PostgreSQL format with %s signifying a parameter placeholder.
+The function clara_database_adapter.localise_sql_query converts this to sqlite3 format if necessary.
+"""
+
+from .clara_database_adapter import connect, localise_sql_query
+
+from .clara_utils import _s3_storage, get_config, absolute_file_name, absolute_local_file_name, file_exists, local_file_exists, copy_local_file, basename
+from .clara_utils import make_directory, remove_directory, directory_exists, local_directory_exists, make_local_directory
+from .clara_utils import list_files_in_directory, post_task_update
+
+from .clara_classes import InternalCLARAError
+
+from pathlib import Path
+
+import os
+import traceback
+
+config = get_config()
+
 class ImageRepository:
     def __init__(self, callback=None):   
         self.db_file = absolute_local_file_name(config.get('image_repository', ('db_file' if _s3_storage else 'db_file_local')))
