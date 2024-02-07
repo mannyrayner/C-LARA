ce20af096a1d1979a2d2cf4e9399707d68332bd2
Manny Rayner <mannyrayner@yahoo.com>
2024-02-07 16:40:29 +1030

    Reading histories

--- clara_app/views.py
+++ clara_app/views.py
@@ -3775,8 +3775,9 @@ def register_project_content_helper(project_id, phonetic_or_normal):
 
 # Show link to reading history for user and l2_language
 # There are controls to change the l2 and add a project to the reading history
+# The 'status' argument is used to display a completion message when we redirect back here after updating the history
 @login_required
-def reading_history(request, l2_language):
+def reading_history(request, l2_language, status):
     user = request.user
     reading_history, created = ReadingHistory.objects.get_or_create(user=user, l2=l2_language)
 
@@ -3793,15 +3794,16 @@ def reading_history(request, l2_language):
         # Changing the reading history language
         if action == 'select_language':
             l2_language =  request.POST['l2']
-            return redirect('reading_history', l2_language)
+            messages.success(request, f"Now on reading history for {l2_language}.")
+            return redirect('reading_history', l2_language, 'init')
 
-        # Deleting the reading history for the currently selected language
+                # Deleting the reading history for the currently selected language
         elif action == 'delete_reading_history':
             reading_history.delete()
             clara_project.delete()
             clara_project_internal.delete()
             messages.success(request, "Reading history deleted successfully.")
-            return redirect('reading_history', l2_language)  
+            return redirect('reading_history', l2_language, 'init')  
 
         # Adding a project to the end of the reading history     
         elif action == 'add_project':
@@ -3820,9 +3822,18 @@ def reading_history(request, l2_language):
                         reading_history_internal = ReadingHistoryInternal(project_id, clara_project_internal, internal_projects_in_history)
 ##                      reading_history_internal.create_combined_text_object()
                         new_project_internal = CLARAProjectInternal(new_project.internal_id, new_project.l2, new_project.l1)
-                        reading_history_internal.add_component_project_and_create_combined_text_object(new_project_internal, phonetic=False)
-                        reading_history_internal.render_combined_text_object()
-                        project_id = clara_project.id
+
+                        task_type = f'update_reading_history'
+                        callback, report_id = make_asynch_callback_and_report_id(request, task_type)
+
+                        async_task(update_reading_history, reading_history_internal, new_project_internal, l2_language, callback=callback)
+
+                        # Redirect to the monitor view, passing the task ID and report ID as parameters
+                        return redirect('update_reading_history_monitor', l2_language, report_id)
+                        
+##                        reading_history_internal.add_component_project_and_create_combined_text_object(new_project_internal, phonetic=False)
+##                        reading_history_internal.render_combined_text_object()
+##                        project_id = clara_project.id
 
                 except Exception as e:
                     messages.error(request, f"Something went wrong when trying to add project to reading history")
@@ -3835,6 +3846,10 @@ def reading_history(request, l2_language):
     # GET request
     # Display the language, the current reading history projects, a link to the compiled reading history, and controls
     else:
+        if status == 'finished':
+            messages.success(request, f'Reading history successfully updated')
+        elif status == 'error':
+            messages.error(request, "Something went wrong when updating the reading history. Try looking at the 'Recent task updates' view")
         languages_available = l2s_in_posted_content()
         projects_in_history = reading_history.get_ordered_projects()
         projects_available = projects_available_for_adding_to_history(l2_language, projects_in_history)
@@ -3842,14 +3857,52 @@ def reading_history(request, l2_language):
         l2_form = L2LanguageSelectionForm(languages_available=languages_available, l2=l2_language)
         add_project_form = AddProjectToReadingHistoryForm(projects_available=projects_available)
 
+    clara_version = get_user_config(request.user)['clara_version']
+
     return render(request, 'clara_app/reading_history.html', {
         'l2_form': l2_form,
         'add_project_form': add_project_form,
         'projects_in_history': projects_in_history,
         # project_id is used to construct the link to the compiled reading history
         'project_id': project_id,
-        'projects_available': projects_available
+        'projects_available': projects_available,
+        'clara_version':clara_version
     })
+
+# Function to call in asynch process. Update and render the CLARAProjectInternal associated with the reading history
+def update_reading_history(reading_history_internal, new_project_internal, l2_language, callback=None):
+    try:
+        reading_history_internal.add_component_project_and_create_combined_text_object(new_project_internal, phonetic=False)
+        reading_history_internal.render_combined_text_object()
+
+        post_task_update(callback, f"finished")
+    except Exception as e:
+        post_task_update(callback, f"Something went wrong when trying to add project to reading history")
+        post_task_update(callback, f"Exception: {str(e)}\n{traceback.format_exc()}")
+        
+        post_task_update(callback, f"error")
+
+@login_required
+def update_reading_history_status(request, l2_language, report_id):
+    messages = get_task_updates(report_id)
+    print(f'{len(messages)} messages received')
+    if 'error' in messages:
+        status = 'error'
+    elif 'finished' in messages:
+        status = 'finished'  
+    else:
+        status = 'unknown'    
+    return JsonResponse({'messages': messages, 'status': status})
+
+# Render the monitoring page, which will use JavaScript to poll the task status API
+@login_required
+def update_reading_history_monitor(request, l2_language, report_id):
+
+    clara_version = get_user_config(request.user)['clara_version']
+    
+    return render(request, 'clara_app/update_reading_history_monitor.html',
+                  {'l2_language':l2_language, 'report_id': report_id, 'clara_version':clara_version})
+
 
 # Create associated CLARAProject and CLARAProjectInternal
 def create_project_and_project_internal_for_reading_history(reading_history, user, l2_language):
