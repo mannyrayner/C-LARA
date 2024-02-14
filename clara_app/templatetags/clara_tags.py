eb5259438a186c8c17132b3eb30a7c7f360af1ef
Manny Rayner <mannyrayner@yahoo.com>
2024-02-14 15:44:21 +1030

    Cleaning up layout

--- clara_app/templatetags/clara_tags.py
+++ clara_app/templatetags/clara_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def zip_lists(a, b):
     return zip(a, b)
+
+@register.filter(name='titlecase')
+def titlecase(value):
+    return value.title()  # Converts to title case
