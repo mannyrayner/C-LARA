895f162564a101538d871ad3744a70a5ae95c24e
Manny Rayner <mannyrayner@yahoo.com>
2024-08-02 22:15:36 +0930

    Access counter

--- clara_app/views.py
+++ clara_app/views.py
@@ -1433,7 +1433,7 @@ def public_content_detail(request, content_id):
 
     # Get the client's IP address
     #client_ip, is_routable = get_client_ip(request, request_header_order=['X_FORWARDED_FOR', 'REMOTE_ADDR'], proxy_count=1)
-    client_ip, is_routable = get_client_ip(request)
+    client_ip, is_routable = get_client_ip(request, request_header_order=['X_FORWARDED_FOR', 'REMOTE_ADDR'])
     
     if client_ip is None:
         client_ip = '0.0.0.0'  # Fallback IP if detection fails
