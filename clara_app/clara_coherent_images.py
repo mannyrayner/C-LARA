608bc14327a40d6207c877afad7ee69ed196b0e0
Manny Rayner <mannyrayner@yahoo.com>
2024-10-03 00:36:17 +0930

    Rewrite of coherent images

--- clara_app/clara_coherent_images.py
+++ clara_app/clara_coherent_images.py
@@ -1,6 +1,4 @@
-import json
-import os
-import asyncio
+
 from .clara_chatgpt4 import (
     get_api_chatgpt4_response,
     get_api_chatgpt4_image_response,
@@ -12,36 +10,71 @@ from .clara_utils import (
     read_json_file,
     write_json_to_file,
     make_directory,
-    absolute_file_name
+    absolute_file_name,
+    file_exists,
+    copy_file
     )
+
+import json
+import os
+import asyncio
+import traceback 
 
 # Test with Lily Goes the Whole Hog
 
 def test_lily_style():
     project_dir = '$CLARA/coherent_images/LilyGoesTheWholeHog'
-    api_calls = asyncio.run(process_style(project_dir))
+    api_calls = asyncio.run(process_style(project_dir, 2, 2))
     cost = sum([api_call.cost for api_call in api_calls])
     print(f'Cost = ${cost:2f}')
     
 # Style
 
-async def process_style(project_dir):
-    expanded_description, api_call1 = await generate_expanded_style_description(project_dir)
-    image_file, api_call2 = await generate_style_image(project_dir, expanded_description)
-    image_description, api_call3 = await interpret_style_image(project_dir, image_file)
-    evaluation, api_call4 = await evaluate_style_fit(project_dir, expanded_description, image_description)
+async def process_style(project_dir, n_expanded_descriptions, n_images_per_description):
+    tasks = []
+    all_description_dirs = []
+    all_api_calls = []
+    for description_version_number in range(0, n_expanded_descriptions):
+        tasks.append(asyncio.create_task(generate_expanded_style_description_and_images(project_dir, description_version_number, n_images_per_description)))
+    results = await asyncio.gather(*tasks)
+    for description_dir, api_calls in results:
+        all_description_dirs.append(description_dir)
+        all_api_calls.extend(api_calls)
+    select_best_expanded_style_description_and_image(project_dir, all_description_dirs)
+    return all_api_calls
 
-    return [ api_call1, api_call2, api_call3, api_call4 ]
+def select_best_expanded_style_description_and_image(project_dir, all_description_dirs):
+    best_score = 0.0
+    best_description_file = None
+    typical_image_file = None
 
-async def generate_expanded_style_description(project_dir):
+    for description_dir in all_description_dirs:
+        image_info_file = project_pathname(project_dir, f'{description_dir}/image_info.json')
+        description_file = project_pathname(project_dir, f'{description_dir}/expanded_description.txt')
+
+        if file_exists(image_info_file) and file_exists(description_file):
+            image_info = read_json_file(image_info_file)
+            if image_info['av_score'] > best_score:
+                best_description_file = description_file
+                typical_image_file = image_info['image']
+
+    if best_description_file and typical_image_file:
+        copy_file(best_description_file, project_pathname(project_dir, f'style/expanded_style_description.txt'))
+        copy_file(typical_image_file, project_pathname(project_dir, f'style/style_image.jpg'))
+            
+
+async def generate_expanded_style_description_and_images(project_dir, description_version_number, n_images_per_description):
     # Make directory if necessary
-    make_project_dir(project_dir, 'style')
+    description_directory = f'style/description_v{description_version_number}'
+    make_project_dir(project_dir, description_directory)
     
     # Read the base style description
-    base_description = read_project_txt_file(project_dir, 'style/style_description.txt')
+    base_description = read_project_txt_file(project_dir, f'style/style_description.txt')
 
     # Get the text of the story
     text = get_text(project_dir)
+
+    all_api_calls = []
 
     # Create the prompt to expand the style description
     prompt = f"""We are later going to create a set of images to illustrate the following text:
@@ -57,26 +90,109 @@ generate a single image, appropriate to the story, which exemplifies the style. 
 most 3000 characters long to conform to DALL-E-3's constraints."""
 
     # Get the expanded description from the AI
-    api_call = await get_api_chatgpt4_response(prompt)
+    try:
+        valid_expanded_description_produced = False
+        tries_left = 5
+        max_dall_e_3_prompt_length = 4000
+        
+        while not valid_expanded_description_produced and tries_left:
+            description_api_call = await get_api_chatgpt4_response(prompt)
+            all_api_calls.append(description_api_call)
 
-    # Save the expanded description
-    expanded_description = api_call.response
-    write_project_txt_file(expanded_description, project_dir, 'style/expanded_description.txt')
-    return expanded_description, api_call
+            # Save the expanded description
+            expanded_description = description_api_call.response
+            if len(expanded_description) < max_dall_e_3_prompt_length:
+                valid_expanded_description_produced = True
+            else:
+                tries_left -= 1
+            
+        write_project_txt_file(expanded_description, project_dir, f'style/description_v{description_version_number}/expanded_description.txt')
 
-async def generate_style_image(project_dir, description):
-    # Make directories if necessary
-    make_project_dir(project_dir, 'style/images')
-    make_project_dir(project_dir, 'style/descriptions')
+        # Create and rate the images
+        image_api_calls = await generate_and_rate_style_images(project_dir, expanded_description, description_version_number, n_images_per_description)
+        all_api_calls.extend(image_api_calls)
+
+        return description_directory, all_api_calls
+
+    except Exception as e:
+        error_message = f'"{str(e)}"\n{traceback.format_exc()}'
+        write_project_txt_file(error_message, project_dir, f'style/description_v{description_version_number}/error.txt')
+
+    return description_directory, []
+
+async def generate_and_rate_style_images(project_dir, description, description_version_number, n_images_per_description):
+    description_dir = f'style/description_v{description_version_number}'
+    make_project_dir(project_dir, description_dir)
     
-    image_file = project_pathname(project_dir, 'style/images/style_image.jpg')
+    tasks = []
+    all_image_dirs = []
+    all_api_calls = []
+    for image_version_number in range(0, n_images_per_description):
+        tasks.append(asyncio.create_task(generate_and_rate_style_image(project_dir, description, description_version_number, image_version_number)))
+    results = await asyncio.gather(*tasks)
+    for image_dir, api_calls in results:
+        all_image_dirs.append(image_dir)
+        all_api_calls.extend(api_calls)
+
+    score_description_dir(project_dir, description_dir, all_image_dirs)
+        
+    return all_api_calls
+
+def score_description_dir(project_dir, description_dir, image_dirs):
+    scores_and_images_dirs = [ ( score_for_image_dir(project_dir, image_dir), image_dir )
+                               for image_dir in image_dirs ]
+    scores = [ item[0] for item in scores_and_images_dirs ]
+    av_score = sum(scores) / len(scores)
+
+    # Find the image most representative of the average score
+
+    closest_match = 10.0
+    closest_file = None
+
+    for score, image_dir in scores_and_images_dirs:
+        image_file = project_pathname(project_dir, f'{image_dir}/image.jpg')
+        if abs(score - av_score ) < closest_match and file_exists(image_file):
+            closest_match = abs(score - av_score )
+            closest_file = image_file
+
+    description_dir_info = { 'av_score': av_score,
+                             'image': image_file }
+                                                                   
+    write_project_json_file(description_dir_info, project_dir, f'{description_dir}/image_info.json')
+
+async def generate_and_rate_style_image(project_dir, description, description_version_number, image_version_number):
+    all_api_calls = []
+    
+    image_dir = f'style/description_v{description_version_number}/image_v{image_version_number}'
+    # Make directory if necessary
+    make_project_dir(project_dir, image_dir)
+
+    try:
+        image_file, image_api_call = await generate_style_image(project_dir, image_dir, description)
+        all_api_calls.append(image_api_call)
+
+        image_interpretation, interpret_api_call = await interpret_style_image(project_dir, image_dir, image_file)
+        all_api_calls.append(interpret_api_call)
+
+        evaluation, evaluation_api_call = await evaluate_style_fit(project_dir, image_dir, description, image_interpretation)
+
+        all_api_calls.append(evaluation_api_call)
+
+    except Exception as e:
+        error_message = f'"{str(e)}"\n{traceback.format_exc()}'
+        write_project_txt_file(error_message, project_dir, f'{image_dir}/error.txt')
+        write_project_txt_file("0", project_dir, f'{image_dir}/evaluation.txt')
+
+    return image_dir, all_api_calls
+
+async def generate_style_image(project_dir, image_dir, description):
+    
+    image_file = project_pathname(project_dir, f'{image_dir}/image.jpg')
     api_call = await get_api_chatgpt4_image_response(description, image_file)
 
     return image_file, api_call
 
-async def interpret_style_image(project_dir, image_file):
-    # Make directories if necessary
-    make_project_dir(project_dir, 'style/descriptions')
+async def interpret_style_image(project_dir, image_dir, image_file):
 
     prompt = """Please provide as detailed a description as possible of the following image, focussing
 on the style. The content is not important.
@@ -86,14 +202,12 @@ intended style, and the information you provide will be used to ascertain how go
 """
     api_call = await get_api_chatgpt4_interpret_image_response(prompt, image_file)
 
-    description = api_call.response
-    write_project_txt_file(description, project_dir, 'style/descriptions/style_image_description.txt')
+    image_interpretation = api_call.response
+    write_project_txt_file(image_interpretation, project_dir, f'{image_dir}/image_interpretation.txt')
 
-    return description, api_call
+    return image_interpretation, api_call
 
-async def evaluate_style_fit(project_dir, expanded_description, image_description):
-    # Make directories if necessary
-    make_project_dir(project_dir, 'style/evaluations')
+async def evaluate_style_fit(project_dir, image_dir, expanded_description, image_description):
     
     prompt = f"""Please read the 'style description' and the 'image description' below.
 
@@ -109,27 +223,39 @@ Style Description:
 Image Description:
 {image_description}
 
-Evaluate using one of the following words: excellent/good/acceptable/poor.
+Score the evaluation as a number fron 0 to 4 according to the following conventions:
 
-The response will be read by a Python script, so give only the single-word evaluation."""
+4 = excellent
+3 = good
+2 = clearly accesptable
+1 = possibly acceptable
+0 = unacceptable
+
+The response will be read by a Python script, so write only the single evaluation score."""
     api_call = await get_api_chatgpt4_response(prompt)
 
     evaluation = api_call.response
 
-    write_project_txt_file(evaluation, project_dir, 'style/evaluations/style_evaluation.txt')
+    write_project_txt_file(evaluation, project_dir, f'{image_dir}/evaluation.txt')
     
     return evaluation, api_call
 
+def score_for_image_dir(project_dir, image_dir):
+    try:
+        score = read_project_txt_file(project_dir, f'{image_dir}/evaluation.txt')
+        return int(score)
+    except Exception as e:
+        return 0
 
 def get_pages(project_dir):
-    story_data = read_project_json_file(project_dir, 'story.json')
+    story_data = read_project_json_file(project_dir, f'story.json')
 
     pages = [ item['page_number'] for item in story_data ]
                
     return pages
 
 def get_text(project_dir):
-    story_data = read_project_json_file(project_dir, 'story.json')
+    story_data = read_project_json_file(project_dir, f'story.json')
 
     text_content = [ item['text'] for item in story_data ]
                
