bb6ed90369269a59483bcfcb4336e813bb536ede
Manny Rayner <mannyrayner@yahoo.com>
2025-09-16 21:39:07 +0930

    TQs

--- clara_app/templatetags/dict_extras.py
+++ clara_app/templatetags/dict_extras.py
@@ -3,7 +3,9 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key)
-
- 
+def get_item(d, key):
+    try:
+        return d.get(key)
+    except Exception:
+        return None
+    
